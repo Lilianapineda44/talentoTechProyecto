b0e915d963682a63e75da498dc51babc1a70fc10
Lilianapineda44 <lilianapineda44@gmail.com>
2025-08-09 18:39:39 -0500

    update

---
+++ javascript/consumo_energia_renovable.js
@@ -0,0 +1,8 @@
+var data = [
+
+    {"label" : "Hidroeléctrica" , "value" : 30 },
+    {"label" : "Energia solar" , "value" : 25 },
+    {"label" : "Energia Eólica" , "value" : 35 },
+    {"label" : "Otras" , "value" : 10},
+
+]
---
+++ javascript/produccion_por_fuente.js
@@ -0,0 +1,9 @@
+var data = [
+
+    {"label" : "Hidraulica" , "value" : 6000 },
+    {"label" : "Geotérmica" , "value" : 200},
+    {"label" : "Eolica" , "value" : 4200 },
+    {"label" : "Biocombustible" , "value" : 1800 },
+    {"label" : "Solar" , "value" : 3500 },
+
+]
